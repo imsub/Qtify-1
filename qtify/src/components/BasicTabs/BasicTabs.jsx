6b3ad0155cdc3a40553d0b12193d75cde179b066
subham jalan <subhamj13@gmail.com>
2024-06-27 21:18:11 +0530

    changes

--- qtify/src/components/BasicTabs/BasicTabs.jsx
+++ qtify/src/components/BasicTabs/BasicTabs.jsx
@@ -57,7 +57,8 @@ export default function BasicTabs({ handleChangeIndex}) {
         <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" 
          TabIndicatorProps={{
             style: { backgroundColor: "#34c94b" }
-        }} textColor='#34c94b' >
+        }} 
+       >
           <Tab label="All" {...a11yProps(0)} />
           <Tab label="Rock" {...a11yProps(1)} />
           <Tab label="Pop" {...a11yProps(2)} />
